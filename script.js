c800e517032287eae33743502f03bf55e5bb17cc
samironta <samironta@outlook.com>
2022-02-17 14:31:50 +0000

    initial commmit plus challenge

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("h2").addClass("underline"); // underlines all h2 elements
+    $("ul").addClass("border"); //adds border to all lists
+    // could be removeClass
+    $("tr:odd").addClass("odd");
+    $("tr:even").addClass("even");
+
+});
